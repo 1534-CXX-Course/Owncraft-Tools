221f5b44266982660738577f8334eecc003d26d8
denisr03 <rublev.deni03@mail.ru>
2018-02-07 18:15:17 +0300

    Lesson 3

---
+++ pyramid/pyramid.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <string>
+#include <iosfwd>
+
+enum Orientation // Перечисление
+{
+	UP,
+	DOWN,
+	RIGHT,
+	LEFT,
+	FORWARD,
+	BACKWARD
+};
+
+void up_pyramid
+(
+	int x,
+	int y,
+	int z,
+	int height,
+	std::string block_name,
+	std::ofstream &out
+);
+
+void down_pyramid
+(
+	int x,
+	int y,
+	int z,
+	int height,
+	std::string block_name,
+	std::ofstream &out
+);
+
+void left_pyramid
+(
+	int x,
+	int y,
+	int z,
+	int height,
+	std::string block_name,
+	std::ofstream &out
+);
+
+void right_pyramid
+(
+	int x,
+	int y,
+	int z,
+	int height,
+	std::string block_name,
+	std::ofstream &out
+);
+
+void forward_pyramid
+(
+	int x,
+	int y,
+	int z,
+	int height,
+	std::string block_name,
+	std::ofstream &out
+);
+
+void backward_pyramid
+(
+	int x,
+	int y,
+	int z,
+	int height,
+	std::string block_name,
+	std::ofstream &out
+);
